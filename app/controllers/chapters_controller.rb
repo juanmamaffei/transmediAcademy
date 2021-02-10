5546a2dea934a6a7cbc8295f5fa2c63cd6f2da57
Juanma <juanmamaffei@gmail.com>
2021-02-10 01:56:46 -0300

    Ajuste de permisos para CHAPTERS Los estudiantes pueden LEER e INDEXAR capítulos. Los tutores pueden LEER, INDEXAR, EDITAR y BORRAR capítulos.

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -2,63 +2,120 @@ class ChaptersController < ApplicationController
   before_action :set_chapter, only: %i[ show edit update destroy ]
   before_action :set_course
   before_action :authenticate_user!
+  before_action :check_permissions
+
 
   # GET /chapters or /chapters.json
   def index
-    @chapters = Chapter.where(course_id:@course)
+    # Sólo acceden los alumnos
+    if @permissions >= 10
+      
+      @chapters = Chapter.where(course_id:@course)
+    else
+      #Redirigir
+      redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
+    end
+  
   end
 
   # GET /chapters/1 or /chapters/1.json
   def show
-    @contents = Content.where(course_id: @course.id, chapter_id: @chapter.id).order("sequence ASC, created_at DESC")
+    
+    # Sólo acceden los alumnos
+    if @permissions >= 10
+      
+      @contents = Content.where(course_id: @course.id, chapter_id: @chapter.id).order("sequence ASC, created_at DESC")
+    else
+      #Redirigir
+      redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
+    end
+  
+
+
   end
 
   # GET /chapters/new
   def new
+
+    # Sólo acceden los tutores
+    if @permissions >= 20
+      @chapter = Chapter.new
+    else
+        #Redirigir
+        redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
+    end
     
-    @chapter = Chapter.new
   end
 
   # GET /chapters/1/edit
   def edit
+    # Sólo acceden los tutores
+    unless @permissions >= 20
+      redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
+    end
   end
 
   # POST /chapters or /chapters.json
   def create
-    @chapter = Chapter.new(chapter_params)
-    @chapter.course_id = @course.id
 
-    respond_to do |format|
-      if @chapter.save
-        format.html { redirect_to @chapter.course, notice: "Chapter was successfully created." }
-        format.json { render :show, status: :created, location: @chapter }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @chapter.errors, status: :unprocessable_entity }
+
+    # Sólo acceden los tutores
+    if @permissions >= 20
+      @chapter = Chapter.new(chapter_params)
+      @chapter.course_id = @course.id
+  
+      respond_to do |format|
+        if @chapter.save
+          format.html { redirect_to @chapter.course, notice: "Chapter was successfully created." }
+          format.json { render :show, status: :created, location: @chapter }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @chapter.errors, status: :unprocessable_entity }
+        end
       end
+    else
+        #Redirigir
+        redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
     end
+    
   end
 
   # PATCH/PUT /chapters/1 or /chapters/1.json
   def update
-    respond_to do |format|
-      if @chapter.update(chapter_params)
-        format.html { redirect_to @chapter.course, notice: "Chapter was successfully updated." }
-        format.json { render :show, status: :ok, location: @chapter }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @chapter.errors, status: :unprocessable_entity }
+    # Sólo acceden los tutores
+    if @permissions >= 20
+      respond_to do |format|
+        if @chapter.update(chapter_params)
+          format.html { redirect_to @chapter.course, notice: "Chapter was successfully updated." }
+          format.json { render :show, status: :ok, location: @chapter }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @chapter.errors, status: :unprocessable_entity }
+        end
       end
+    else
+        #Redirigir
+        redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
     end
+
   end
 
   # DELETE /chapters/1 or /chapters/1.json
   def destroy
-    @chapter.destroy
-    respond_to do |format|
-      format.html { redirect_to course_chapters_url, notice: "Chapter was successfully destroyed." }
-      format.json { head :no_content }
+
+
+    # Sólo acceden los tutores
+    if @permissions >= 20
+      @chapter.destroy
+      respond_to do |format|
+        format.html { redirect_to course_chapters_url, notice: "Chapter was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    else
+        #Redirigir
+        redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
     end
+  
   end
 
   private
@@ -75,4 +132,14 @@ class ChaptersController < ApplicationController
       @course = Course.find(params[:course_id])
     end
     
+    def check_permissions
+      matricula = Matriculation.find_by(user: current_user, course: @course)
+
+      if matricula == nil
+        @permissions = 0
+      else
+        @permissions = matricula.permissions
+      end
+  end
+
 end
