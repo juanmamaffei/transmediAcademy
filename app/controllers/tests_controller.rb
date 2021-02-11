81f297eee58f53d5a33262ae4edae106dca424b0
Juanma <juanmamaffei@gmail.com>
2021-02-11 00:30:01 -0300

    Permisos para TESTS

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,9 +2,24 @@ class TestsController < ApplicationController
   before_action :set_test, only: %i[ show edit update destroy ]
   before_action :set_content
 
+  before_action :check_permissions
+
   # GET /tests or /tests.json
   def index
-    @tests = Test.where(content_id: @content.id)
+
+    # Sólo acceden los tutores
+    if @permissions >= 20
+      #Lo que se puede hacer
+      @tests = Test.where(content_id: @content.id)
+    elsif @permissions >= 10
+      #Tomar uno random
+      untest = Test.where(content_id: @content.id).order("RANDOM()").first
+      redirect_to content_test_path(@content, untest)
+    else
+      #Redirigir al root
+      redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
+    end
+
   end
 
   # GET /tests/1 or /tests/1.json
@@ -12,53 +27,100 @@ class TestsController < ApplicationController
     # Si el visitante es alumno y nunca tomó el test,
     # la vista mostrará las preguntas mezcladas pero no la respuesta.
     # Al hacer clic en una de las respuestas, se enviará a CORREGIR
+    
+    # Sólo acceden los estudiantes
+    unless @permissions >= 10
+      redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
+    end
+
   end
 
   # GET /tests/new
   def new
-    @test = Test.new
+    
+    # Sólo acceden los tutores
+    if @permissions >= 20
+      @test = Test.new
+    else
+        #Redirigir
+        redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
+    end
   end
 
   # GET /tests/1/edit
   def edit
+    # Sólo acceden los tutores
+    unless @permissions >= 20
+      redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
+    end
   end
 
   # POST /tests or /tests.json
   def create
-    @test = Test.new(test_params)
-    @test.content_id = params[:test][:content]
+    # Sólo acceden los tutores
+    if @permissions >= 20
+      
 
-    respond_to do |format|
-      if @test.save
-        format.html { redirect_to content_test_path(@content, @test), notice: "Creaste una prueba ;-)" }
-        format.json { render :show, status: :created, location: @test }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @test.errors, status: :unprocessable_entity }
+      @test = Test.new(test_params)
+      @test.content_id = params[:test][:content]
+  
+      respond_to do |format|
+        if @test.save
+          format.html { redirect_to content_test_path(@content, @test), notice: "Creaste una prueba ;-)" }
+          format.json { render :show, status: :created, location: @test }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @test.errors, status: :unprocessable_entity }
+        end
       end
+
+    else
+        #Redirigir
+        redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
     end
+    
+    
+
   end
 
   # PATCH/PUT /tests/1 or /tests/1.json
   def update
-    respond_to do |format|
-      if @test.update(test_params)
-        format.html { redirect_to content_test_path(@content, @test), notice: "Modificaste una prueba. Ya no se van a machetear..." }
-        format.json { render :show, status: :ok, location: @test }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @test.errors, status: :unprocessable_entity }
+
+    # Sólo acceden los tutores
+    if @permissions >= 20
+      
+      respond_to do |format|
+        if @test.update(test_params)
+          format.html { redirect_to content_test_path(@content, @test), notice: "Modificaste una prueba. Ya no se van a machetear..." }
+          format.json { render :show, status: :ok, location: @test }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @test.errors, status: :unprocessable_entity }
+        end
       end
+    else
+        #Redirigir
+        redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
     end
+
+
   end
 
   # DELETE /tests/1 or /tests/1.json
   def destroy
-    @test.destroy
-    respond_to do |format|
-      format.html { redirect_to content_tests_path, notice: "Borraste una prueba." }
-      format.json { head :no_content }
-    end
+
+      # Sólo acceden los tutores
+      if @permissions >= 20
+        @test.destroy
+        respond_to do |format|
+          format.html { redirect_to content_tests_path, notice: "Borraste una prueba." }
+          format.json { head :no_content }
+        end
+      else
+          #Redirigir
+          redirect_to root_path, notice: "No tenés permiso para esto, chinwenwencha."
+      end
+
   end
   
   # POST corregir/1
@@ -94,4 +156,19 @@ class TestsController < ApplicationController
     def test_params
       params.require(:test).permit(:question, :a1, :a2, :a3, :a4, :correct, :content_id, :score, :difficulty, :state)
     end
+
+    def check_permissions
+      
+      cap = Content.find_by(id: params[:content_id])
+      course = Course.find(cap.course_id)
+
+      matricula = Matriculation.find_by(user: current_user, course: course)
+
+      if matricula == nil
+        @permissions = 0
+      else
+        @permissions = matricula.permissions
+      end
+  end
+
 end
